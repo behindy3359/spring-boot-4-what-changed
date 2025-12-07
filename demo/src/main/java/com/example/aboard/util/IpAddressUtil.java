b7b616679f537b4755a4714fd996fe54c723ca77
behindy3359 <solme47@gmail.com>
2025-12-07 16:45:20 +0900

    feat : ip 추출 유틸

---
+++ demo/src/main/java/com/example/aboard/util/IpAddressUtil.java
@@ -0,0 +1,30 @@
+package com.example.aboard.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class IpAddressUtil {
+
+    public static String getClientIp(HttpServletRequest request){
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if ("0:0:0:0:0:0:0:1".equals(ip) || "127.0.0.1".equals(ip)) {
+            ip = "127.0.0.1";
+        }
+        return ip;
+    }
+}
